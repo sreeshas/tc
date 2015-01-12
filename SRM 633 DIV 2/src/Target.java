fa3bd7cdc1320abfce70bdd0f27c1f7b91297933
Sreenidhi Sreesha <sreenidhibs@gmail.com>
2015-01-11 23:49:20 -0800

    SRM 633 DIV2

---
+++ SRM 633 DIV 2/src/Target.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.math.*;
+import static java.lang.Math.*;
+
+public class Target {
+	
+	public String[] draw(int n) {
+		char[][] target = new char[n][n];
+		init(target, n);
+		char[] symbol = new char[]{'#', ' '};
+		int sym = 0;
+		int k = 0 ;
+		target[n/2][n/2] = '#';
+		while(k < n/2) {
+			drawAround(target,k, n-1-k, k, n-1-k,symbol[k%2], n);
+			k++;
+
+		}
+		return draw2(target, n);
+	}
+	public static void drawAround(char[][] target, int row1, int row2, int col1, int col2, char symbol, int n) {
+		for(int i=col1; i<=col2; i++) {
+			target[row1][i] = symbol;
+		}
+		for(int i= row1; i<=row2; i++) {
+			target[i][col2] = symbol;
+		}
+		for(int i=col1; i<=col2; i++) {
+			target[row2][i] = symbol;
+		}
+		for(int i=row1; i<=row2; i++) {
+			target[i][col1] = symbol;
+		}
+
+	}
+
+	public static String[] draw2(char[][] target, int n) {
+		String[] result = new String[n];
+		for(int i=0; i< n; i++) {
+			String answer = "";
+			for(int j=0;j<n;j++) {
+				answer += target[i][j];
+			}
+			result[i] = answer;
+		}
+		return result;
+	}
+	public static void init(char[][] target, int n) {
+		for(int i=0; i< n; i++) {
+			for(int j=0;j<n;j++) {
+				target[i][j] = '.';
+			}
+
+		}
+	}
+}
