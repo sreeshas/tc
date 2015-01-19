27849857086a87dc55f11bc9f151f83f99a7db2f
Sreenidhi Sreesha <sreenidhibs@gmail.com>
2015-01-18 23:10:17 -0800

    SRM 219 DIV2 Couldnt figure this one out. floating point arithmetic is tricky.

--- SRM 219 DIV 2/src/WaiterTipping.java
+++ SRM 219 DIV 2/src/WaiterTipping.java
@@ -3,17 +3,12 @@ import java.math.*;
 import static java.lang.Math.*;
 
 public class WaiterTipping {
-	
 	public int maxPercent(int total, int taxPercent, int money) {
-		int result = 0;
-        float tax = (total * taxPercent)/100;
-		float cost = total + tax;
-		if (money < cost) {
-			return -1;
-		}
-		float tip = money - cost;
-		int tipp = Math.round((tip /total) * 100);
 
-		return tipp;
+		int tip =0;
+		while(total + Math.floor(total*taxPercent/100) + Math.floor(total*tip/100) <=money){
+			tip++;
+		}
+		return tip-1;
 	}
 }
