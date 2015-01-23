501d942b560eda9b393b5aae462dda5edd63d32e
Sreenidhi Sreesha <sreenidhibs@gmail.com>
2015-01-22 23:44:59 -0800

    SRM 464 DIV2 Could not solve on time.

--- SRM 464 DIV 2/src/ColorfulBoxesAndBalls.java
+++ SRM 464 DIV 2/src/ColorfulBoxesAndBalls.java
@@ -5,19 +5,20 @@ import static java.lang.Math.*;
 public class ColorfulBoxesAndBalls {
 	
 	public int getMaximum(int numRed, int numBlue, int onlyRed, int onlyBlue, int bothColors) {
-        int[] redboxes = new int[numRed];
-		int[] blueboxes = new int[numBlue];
+
 		int redballs = numRed;
 		int blueballs= numBlue;
         //fill redballs
-		int redballcount = redballs;
-		int blueballcount;
-		int result = 0;
-		while(redballcount >0) {
-			if( blue)
-			result = result+ max(onlyRed, bothColors);
+		int result1 =  (numRed*onlyRed) + (numBlue*onlyBlue);
+		int result2 = 0;
+		if (numRed > numBlue) {
+			result2 = (numRed - numBlue)*onlyRed + numBlue * bothColors + numBlue*bothColors;
+		} else if (numBlue > numRed){
+			result2 = (numBlue -numRed)*onlyBlue + numRed * bothColors + numRed *bothColors;
+		} else {
+			result2 = numBlue*bothColors+numBlue*bothColors;
 		}
 
- 		return 0;
+ 		return max(result1, result2);
 	}
 }
